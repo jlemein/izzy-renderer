cf889df98ec7949dca1db55dacd3e096007c12c8
Jeffrey Lemein <jeffrey.lemein@gmail.com>
2020-12-12 10:00:22 +0100

    Using conan for our build now

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+#rm -rf ./build/* && mkdir build
+
+# we are using GCC compile >= 5.1, so we need to set the compiler.libcxx to the new ABI.
+conan profile new default --detect
+conan profile update settings.compiler.libcxx=libstdc++11 default
+conan install . -if build
+
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -B build
 cmake --build ./build --config Release -j2
