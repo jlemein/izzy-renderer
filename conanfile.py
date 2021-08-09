b514f9df2487591329e9fff79f5a5b9c3d82e125
Jeffrey Lemein <jeffrey.lemein@ibeo-as.com>
2021-08-09 07:59:13 +0200

    Updated conan approach

---
+++ conanfile.py
@@ -0,0 +1,57 @@
+from conans import ConanFile, CMake, tools
+from conan.tools.cmake import CMakeDeps
+
+
+class RendererConan(ConanFile):
+    name = "renderer"
+    version = "0.1"
+    license = "<Put the package license here>"
+    author = "Jeffrey Lemein"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Renderer here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False]
+    }
+    generators = "cmake", "cmake_find_package"
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "gtest:build_gmock": True
+    }
+
+    # def configure(self):
+    #     cmake = CMakeDeps(self)
+    #     cmake.build_context_activated = ["gtest"]
+    #     cmake.generate()
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder=".")
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    # def build_requirements(self):
+
+    def requirements(self):
+        self.requires("gtest/1.11.0")
+        self.requires("assimp/5.0.1")
+        self.requires("glm/0.9.9.8")
+        self.requires("entt/3.7.1")
+        self.requires("glfw/3.3.2")
+        self.requires("spdlog/1.8.2")
+        self.requires("resourcemanager/0.2@lemonsoftware/dev")
+        self.requires("imgui/1.80")
+        self.requires("nlohmann_json/3.9.1")
+        self.requires("stb/20200203")
+        self.requires("boost/1.76.0")
+
