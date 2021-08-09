d00bf92c1030b7296a1cad8566dff656d4e9ebbd
Jeffrey Lemein <jeffrey.lemein@gmail.com>
2021-08-09 22:15:43 +0200

    Added multisampling, added animation system w. LocalRotation component. Fixed light direction transformation

--- lib/georm/src/anim_animationsystem.cpp
+++ lib/georm/src/anim_animationsystem.cpp
@@ -2,9 +2,30 @@
 // Created by jlemein on 09-08-21.
 //
 #include <anim_animationsystem.h>
+#include <anim_localrotation.h>
 #include <ecs_rendersubsystem.h>
+#include <ecs_transform.h>
+#include <ecsg_scenegraph.h>
+#include <iostream>
 using namespace lsw::anim;
+using namespace lsw::ecs;
 
-AnimationSystem::AnimationSystem() {}
+AnimationSystem::AnimationSystem(std::shared_ptr<ecsg::SceneGraph> sceneGraph)
+: m_sceneGraph{sceneGraph}
+{}
 
+void AnimationSystem::init() {}
+
+void AnimationSystem::update(float time, float dt) {
+  auto& registry = m_sceneGraph->getRegistry();
+  auto view = registry.view<anim::LocalRotation, Transform>();
+
+  for (auto e : view) {
+    auto &t = registry.get<Transform>(e);
+    auto &rot = view.get<LocalRotation>(e);
+
+    // TODO: make quaternion operation
+    t.localTransform *= glm::rotate(glm::mat4(1.F), rot.radiansPerSecond*dt, rot.axis);
+  }
+}
 
